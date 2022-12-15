9308d1458bce6cf7b18e705ffc8c0df8efe0ee31
Brett Austin Eastman <brett.austin.eastman@gmail.com>
2022-12-14 16:58:27 -0800

    Add completed versions of functional queue and stack

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,19 +1,49 @@
+// I
+// O
+// C
+// E
+
+// Justification
 var Queue = function() {
   var someInstance = {};
 
   // Use an object with numeric keys to store values
   var storage = {};
 
+  // store variable for beginning and end
+  var firstInd = 0;
+  var lastInd = -1;
+
+  someInstance.storage = storage;
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    // enqueue will be to add the last one to the back of the pack
+    // in other words, add to storage numerically with enqueue, then later use dequeue to remove from the front
+    storage[lastInd + 1] = value;
+    lastInd++;
   };
 
   someInstance.dequeue = function() {
+    // remove from the front of the stack
+    var result = storage[firstInd];
+    delete storage[firstInd];
+    firstInd++;
+    return result;
   };
 
   someInstance.size = function() {
+    return Object.keys(storage).length;
   };
 
   return someInstance;
 };
+
+var newQueue = Queue();
+
+newQueue.enqueue('word');
+newQueue.enqueue('word2');
+newQueue.enqueue('word3');
+console.log(newQueue.storage);
+newQueue.dequeue();
+console.log(newQueue.storage);
